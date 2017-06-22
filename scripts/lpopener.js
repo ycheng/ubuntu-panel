dea5e3eec54903b45472269c249dead0cca36c03
YuanChenCheng <ycheng.tw@gmail.com>
2017-06-22 20:48:16 +0800

    open multiple bug and err checking

--- scripts/lpopener.js
+++ scripts/lpopener.js
@@ -5,8 +5,16 @@ onload = function() {
   if (openlpbug) {
     openlpbug.onclick = function() {
       var urlprefix = "https://bugs.launchpad.net/bugs/";
-      var url = urlprefix + parseInt(bugids.value);
-      chrome.tabs.create({'url': url});
+      var idsar = bugids.value.split(" ")
+        .map(function(x) {return parseInt(x);})
+        .filter(function(x) {return x > 0});
+      var urlsar = idsar
+        .map(function(x) {return urlprefix + x.toString();});
+      if (urlsar.length > 1) {
+        chrome.windows.create({url: urlsar});
+      } else if (urlsar.length > 0) {
+        chrome.tabs.create({'url': urlsar[0]});
+      }
     };
   }
 }
